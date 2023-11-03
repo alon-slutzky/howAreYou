94b9d05cdf95b6c38c239ed45380885d3c51d6fc
alon-slutzky <alon.slutzky@paradox.ai>
2023-11-03 17:16:14 +0200

    lala

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-// List of responses
-const responses = [
-    "Surviving, not thriving.",
-    "Just taking it one day at a time.",
-    "You don't wanna know.",
-    "Better than yesterday, worse than tomorrow.",
-    // Add as many as you like
-  ];
-  
-  // Generate a random response
-  function generateResponse() {
-    const randomIndex = Math.floor(Math.random() * responses.length);
-    document.getElementById("response").innerText = responses[randomIndex];
-  }
-  
-  // Attach the function to the button
-  document.getElementById("generate").addEventListener("click", generateResponse);
-  
+const btn = document.getElementById('generateBtn');
+const responseEl = document.getElementById('response');
+
+let responses = [];
+
+// Fetch the responses from the JSON file
+fetch('responses.json')
+  .then(response => response.json())
+  .then(data => {
+    responses = data.responses;
+
+    btn.addEventListener('click', () => {
+        // Fade out the current text
+        responseEl.style.opacity = '0';
+
+        setTimeout(() => {
+            // Get a new random response
+            const randomResponse = responses[Math.floor(Math.random() * responses.length)];
+            
+            // Set the new response text
+            responseEl.textContent = randomResponse;
+
+            // Fade in the new text
+            responseEl.style.opacity = '1';
+        }, 250); // This delay matches the 0.5s transition time in the CSS
+    });
+  })
+  .catch(error => {
+    console.error("Error fetching the responses:", error);
+  });
